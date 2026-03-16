f394a14ee5e73932b575a921eff85606cbb18c6c
zhangchunsheng <zhangchunsheng423@gmail.com>
2026-03-16 20:01:15 +0800

    update

--- update.sh
+++ update.sh
@@ -1,15 +1,4 @@
 #! /bin/bash
 # git commit -a -m 'update'
-git checkout main
-git commit --no-verify -a -m 'update'
-git push -u origin main
-npm run build
-git checkout gh-pages
-rm -rf assets/*
-cp -r dist/* .
-git add assets
-git commit -a -m 'update'
-git push -u origin gh-pages
-git checkout main
 git commit --no-verify -a -m 'update'
 git push -u origin main
